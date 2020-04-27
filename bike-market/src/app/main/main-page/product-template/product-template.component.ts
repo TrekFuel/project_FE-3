12a2f753196c87e487c8eb657375186e16443f75
TrekFuel <ridge96@yandex.ru>
2020-04-27 09:39:32 +0300

    feat(project): add routing features

--- bike-market/src/app/main/main-page/product-template/product-template.component.ts
+++ bike-market/src/app/main/main-page/product-template/product-template.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-template',
@@ -34,10 +35,14 @@ export class ProductTemplateComponent implements OnInit {
     },
   ];
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+  }
+
+  onProductClick() {
+    this.router.navigate(['/product/:id']);
   }
 
 }
