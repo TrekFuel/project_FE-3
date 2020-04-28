c536ae41f259ed96e5182cf09261ee21aa325b68
TrekFuel <ridge96@yandex.ru>
2020-04-28 16:31:15 +0300

    fix(project): fix content, templates and layout
    Signed-off-by: TrekFuel <ridge96@yandex.ru>

--- bike-market/src/app/shared/product-model.ts
+++ bike-market/src/app/shared/product-model.ts
@@ -1,10 +1,13 @@
 export interface Product {
   id: number;
-  img: string;
   title: string;
   category: string;
   subcategory: string;
   condition: string;
-  price: string;
+  price: number;
+  trade: string;
   city: string;
+  contacts: string;
+  text: string;
+  img: string;
 }
