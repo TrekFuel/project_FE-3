42348a5cecef67947f2f8de824b56711a88c258d
TrekFuel <ridge96@yandex.ru>
2020-05-06 00:23:20 +0300

    delete copy
    Signed-off-by: TrekFuel <ridge96@yandex.ru>

--- bike-market/src/app/main/main-page/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './main-page.component';
-export * from './filters-column/filters-column.component';
-export * from './filters-column/subfilters-column/subfilters-column.component';
-export * from './product-template/product-template.component';
-
